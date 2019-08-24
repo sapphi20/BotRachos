4995503cfafa273f295c80af3492624216a939a0
terrypls <selvastian@gmail.com>
2019-08-24 15:54:31 -0400

    utils para sacar json

--- src/Utils.py
+++ src/Utils.py
@@ -8,3 +8,9 @@ def json_save(db, key, value):
 
     with open(db, "w+") as f:
         f.write(json.dumps(data))
+
+
+def json_get(db, group, key):
+    with open(db, "r")as d:
+        data = json.load(d)
+    return data[group][key]
