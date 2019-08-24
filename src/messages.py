4167895a7205643ea2155d913ab7c9f8f7af0cd8
Fabiola RIvera <fabiolarivera@ug.uchile.cl>
2019-08-24 14:56:28 -0400

    Added Json "Datos curiosos" Added images to the resource package Added messages "estado de ebriedad" "estado influencia" and "sanciones"

--- src/messages.py
+++ src/messages.py
@@ -4,3 +4,29 @@ START_CMD_GROUP = "Hola! Los voy a ayudar a tomar responsablemente :D"
 START_CMD_CHANNEL = "Hola! De verdad no sé como me añadiste a un canal, " \
                     "pero necesito estar en un grupo para funcionar"
 HELP_CMD = "Saludos cordiales"
+
+ESTADO_INFLUENCIA = "Bajo la influencia del Alcohol:" \
+                    "0,3 e inferior a 0,8 g/ml." \
+                    "Equivalencia en Tragos:" \
+                    "150 ml de Vino o Champaña de 12° (1 copa)" \
+                    "350 ml de Cerveza (1 Lata)" \
+                    "75 ml de Piscola (0.5 Vasos)" \
+                    "400 ml de Terremoto (2 vasos)*" \
+                    "45 ml de Brandy de 38°" \
+                    "* Valor valido para mujer" \
+                    "** Equivalencia calculada para una mujer de 55Kg y un hombre de 70Kg." \
+                    "*** 1 vaso equivale a 200 ml"
+
+ESTADO_EBRIEDAD = "Estado de ebriedad:" \
+                  "Mayor a 0,8 g/ml" \
+                  "Equivalencia en Tragos:" \
+                  "300 ml de Vino o Champaña de 12° (1 copa)" \
+                  "600 ml de Cerveza (1 Lata)" \
+                  "150 ml de Piscola (0.5 Vasos)" \
+                  "800 ml de Terremoto (2 vasos)*" \
+                  "90 ml de Brandy de 38°" \
+                  "* Valor valido para mujer" \
+                  "** Equivalencia calculada para una mujer de 55Kg y un hombre de 70Kg." \
+                  "*** 1 vaso equivale a 200 ml"
+
+SANCIONES = "resources/SancionesLeyToleranciaCero.jpg"
